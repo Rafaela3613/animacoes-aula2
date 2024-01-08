2f191d47862ab0df3d0791e80856f0ae2bf8e34d
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2024-01-08 18:23:43 -0300

    Utilizando o BehaviorSubject

--- src/app/app-route-reuse-strategy.ts
+++
@@ -1,8 +0,0 @@
-import {ActivatedRouteSnapshot, BaseRouteReuseStrategy} from '@angular/router';
-
-export class AppRouteReuseStrategy extends BaseRouteReuseStrategy {
-
-  public override shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
-    return future.data['reuseComponent'];
-  }
-}
