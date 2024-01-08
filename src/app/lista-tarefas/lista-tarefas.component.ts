c42cad1cad2789507a42463d3558a1ae09762284
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2024-01-08 16:30:15 -0300

    criando o buscador de tarefas

--- src/app/lista-tarefas/lista-tarefas.component.ts
+++ src/app/lista-tarefas/lista-tarefas.component.ts
@@ -5,6 +5,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TarefaService } from 'src/app/service/tarefa.service';
 import { Tarefa } from '../interface/tarefa';
 import { checkButtonTrigger, highlightedStateTrigger, shownStateTrigger } from '../animations';
+import { includes } from 'lodash';
 
 @Component({
   selector: 'app-lista-tarefas',
@@ -23,6 +24,8 @@ export class ListaTarefasComponent implements OnInit {
   validado: boolean = false;
   indexTarefa: number = -1;
   id: number = 0;
+  campoBusca: string = '';
+  tarefasFiltradas: Tarefa[] = [];
 
   formulario: FormGroup = this.fomBuilder.group({
     id: [0],
@@ -41,8 +44,21 @@ export class ListaTarefasComponent implements OnInit {
   ngOnInit(): Tarefa[] {
     this.service.listar(this.categoria).subscribe((listaTarefas) => {
       this.listaTarefas = listaTarefas;
+      this.tarefasFiltradas = listaTarefas;
+
     });
-    return this.listaTarefas;
+    return this.tarefasFiltradas;
+  }
+
+
+  filtrarTarefasPorDescricao(descricao: string) {
+    this.campoBusca = descricao.trim().toLowerCase();
+    if (descricao) {
+      this.tarefasFiltradas = this.listaTarefas.filter(tarefa =>
+        tarefa.descricao.toLowerCase().includes(this.campoBusca));
+    } else {
+      this.tarefasFiltradas = this.listaTarefas;
+    }
   }
 
   mostrarOuEsconderFormulario() {
@@ -125,7 +141,7 @@ export class ListaTarefasComponent implements OnInit {
 
   listarAposCheck() {
     this.service.listar(this.categoria).subscribe((listaTarefas) => {
-      this.listaTarefas = listaTarefas;
+      this.tarefasFiltradas = listaTarefas;
     });
   }
 
